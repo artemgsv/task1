32313a7283211d30c8fbbc6d7e159341ce9ff2de
artemgsv <ar.gusev@yandex.ru>
2021-12-12 18:19:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 // const __dirname = path.resolve();
 
-const app = express();
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
